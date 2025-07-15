546cba4f70611cd922117a926f6a6f724d8f031c
jefrong <62530003+jefrong@users.noreply.github.com>
2025-07-15 19:59:05 +0800

    Delete examples/dotnet directory

--- examples/dotnet/SeleniumDocs/Elements/LocatorsTest.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace SeleniumDocs.Elements
-{
-    [TestClass]
-    public class LocatorsTest : BaseTest
-    {
-    }
-}
--- examples/dotnet/SeleniumDocs/Interactions/AlertsTest.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace SeleniumDocs.Interactions
-{
-    [TestClass]
-    public class AlertsTest : BaseTest
-    {
-    }
-}
